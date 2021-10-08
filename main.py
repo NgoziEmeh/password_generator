140dad5da2389cae6298df8323f688209682d8ad
NgoziEmeh <zingo1ng@yahoo.com>
2021-10-08 15:25:47 -0700

    set flow

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-user_letters= int(input("How many letters would you like in your password?\n"))
+user_letters = int(input("How many letters would you like in your password?\n"))
 user_symbols = int(input(f"How many symbols would you like?\n"))
 user_numbers = int(input(f"How many numbers would you like?\n"))
 
 # generate letters to the number the user wants to add
+letters
 # generate symbols to the number the user wants to add
 # generate numbers to the number the user wants to add
 # shuffle result
