f0f3450a29805751d720242b259c0c76d37f0a7b
NgoziEmeh <zingo1ng@yahoo.com>
2021-10-09 02:27:16 -0700

    loops

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #Password Generator Project
+
 import random
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -10,7 +12,15 @@ user_symbols = int(input(f"How many symbols would you like?\n"))
 user_numbers = int(input(f"How many numbers would you like?\n"))
 
 # generate letters to the number the user wants to add
-letters
+password = ""
+for num in range(1, user_letters + 1):
+    selected_letters = random.choice(letters)
+    password += selected_letters
+for num1 in range(1, user_symbols + 1):
+    selected_symbols = random.choice(symbols)
+    password += selected_symbols
+print(password)
+
 # generate symbols to the number the user wants to add
 # generate numbers to the number the user wants to add
 # shuffle result
